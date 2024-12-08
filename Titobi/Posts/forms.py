d1cdf13ca8d48bcb95057f1392dead58cd394784
SOURCE-CODEcommunity <sourcecode484@gmail.com>
2024-12-09 00:14:26 +0100

    Delete Titobi directory

--- Titobi/Posts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from . import models
-
-class CreatePosts(forms.ModelForm):
-    class Meta:
-        model = models.post
-        fields = ['title', 'body', 'slug', 'banner']
