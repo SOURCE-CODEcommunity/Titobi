06a3d640f62005cdea39bcc7ea11505229bba985
SOURCE-CODEcommunity <sourcecode484@gmail.com>
2024-12-09 01:42:44 +0100

    Update settings.py

--- Titobi/Titobi/settings.py
+++ Titobi/Titobi/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Titobi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
